111c9e3fd4e1f1e2e6d153c771ef099e345d49bf
Anglain <shudra.ia@gmail.com>
2017-06-02 00:52:55 +0300

    Я сделяль тестовый скрипт HowToWinGameJam.cs

---
+++ GameJam_Science/Assets/HowToWinGameJam.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HowToWinGameJam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
