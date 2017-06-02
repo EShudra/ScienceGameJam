97ed52e6f539250e3e7ca66af31a8194f370c956
Anglain <shudra.ia@gmail.com>
2017-06-03 00:04:47 +0300

    Heart.cs changes
    Some minor Heart.cs changes

--- GameJam_Science/Assets/Scripts/Heart.cs
+++ GameJam_Science/Assets/Scripts/Heart.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Heart : Interactive {
 
 	private int slime = 100;
-	private int step = 2;
+	private int step = 1;
 
 	// Use this for initialization
 	protected override void Start () {
@@ -17,20 +17,31 @@ public class Heart : Interactive {
 
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			Debug.Log ("Up key was pressed.");
-			if (Move (0, step, out hit))
-				AttemptMove<Component> (0, step);
+
+			while (Input.GetKey (KeyCode.UpArrow))
+				if (Move (0, step, out hit))
+					AttemptMove<Component> (0, step);
+			
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			Debug.Log ("Down key was pressed.");
-			if (Move (0, -step, out hit))
-				AttemptMove<Component> (0, -step);
+
+			while (Input.GetKey (KeyCode.DownArrow))
+				if (Move (0, -step, out hit))
+					AttemptMove<Component> (0, -step);
+			
 		} else if (Input.GetKeyDown (KeyCode.LeftArrow)){
 			Debug.Log ("Left key was pressed.");
-			if (Move (-step, 0, out hit))
-				AttemptMove<Component> (-step, 0);
+
+			while (Input.GetKey (KeyCode.LeftArrow))
+				if (Move (-step, 0, out hit))
+					AttemptMove<Component> (-step, 0);
+			
 		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
 			Debug.Log ("Right key was pressed.");
-			if (Move (step, 0, out hit))
-				AttemptMove<Component> (step, 0);
+
+			while (Input.GetKey (KeyCode.RightArrow))
+				if (Move (step, 0, out hit))
+					AttemptMove<Component> (step, 0);
 		}
 	}
 
