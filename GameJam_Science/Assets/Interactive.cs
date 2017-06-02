17cec924aed454580e0917807288f89cbe01156e
EShudra <poorgenmail@rambler.ru>
2017-06-02 20:37:09 +0300

    Interactive update
    templates for Move() and AttemptMove()

---
+++ GameJam_Science/Assets/Interactive.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Interactive : MonoBehaviour {
+
+	public float movSpeed = 5;
+
+	protected bool Move(float deltaX, float deltaY, out RaycastHit2D hit){
+		
+	}
+
+	protected void AttemptMove<T>(float deltaX, float deltaY){
+
+
+	}
+
+}
