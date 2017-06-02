8837ba778a61b1e790995fb5648a2394ccaef1bc
Anglain <shudra.ia@gmail.com>
2017-06-02 23:53:14 +0300

    Heart move (1)
    Heart.cs changes
    Heart now can move!
    Heart object is now on a pink background.

--- GameJam_Science/Assets/Scripts/Heart.cs
+++ GameJam_Science/Assets/Scripts/Heart.cs
@@ -4,22 +4,33 @@ using System.Collections;
 public class Heart : Interactive {
 
 	private int slime = 100;
+	private int step = 2;
 
 	// Use this for initialization
-	void Start () {
-
+	protected override void Start () {
+		base.Start ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		RaycastHit2D hit;
+
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			Debug.Log ("Up key was pressed.");
+			if (Move (0, step, out hit))
+				AttemptMove<Component> (0, step);
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			Debug.Log ("Down key was pressed.");
+			if (Move (0, -step, out hit))
+				AttemptMove<Component> (0, -step);
 		} else if (Input.GetKeyDown (KeyCode.LeftArrow)){
 			Debug.Log ("Left key was pressed.");
+			if (Move (-step, 0, out hit))
+				AttemptMove<Component> (-step, 0);
 		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
 			Debug.Log ("Right key was pressed.");
+			if (Move (step, 0, out hit))
+				AttemptMove<Component> (step, 0);
 		}
 	}
 
