43451c058f6fe930282ab7453108c07d3866de94
Anglain <shudra.ia@gmail.com>
2017-06-03 20:30:52 +0300

    CameraBehaviour.cs major changes
    Camera now follows the player and is controlled by the mouse.
    Added inactive tower sprite to the Assets\Sprites\

--- GameJam_Science/Assets/Scripts/CameraBehaviour.cs
+++ GameJam_Science/Assets/Scripts/CameraBehaviour.cs
@@ -3,19 +3,23 @@ using System.Collections;
 
 public class CameraBehaviour : MonoBehaviour {
 	public Transform target;
-	public float smoothing = 5f;
+	//public float smoothing = 5f;
+	public float speed = 20f;
 
 	private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-		
-		offset = transform.position - target.position;
+		//offset = transform.position - target.position;
+		Vector3 trPos = new Vector3 (target.position.x, target.position.y, -10);
+		transform.position = trPos;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		Vector3 targetCameraPosition = target.position + offset;
-		transform.position = Vector3.Lerp (transform.position,targetCameraPosition,smoothing * Time.deltaTime);
+		//Vector3 targetCameraPosition = target.position + offset;
+		//transform.position = Vector3.Lerp (transform.position,targetCameraPosition,smoothing * Time.deltaTime);
+
+		transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed, 0f);
 	}
 }
