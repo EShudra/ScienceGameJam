10381aa068b801361e1247e18e14bcab7af29466
Anglain <shudra.ia@gmail.com>
2017-06-03 20:33:50 +0300

    CameraBehaviour.cs cleanup

--- GameJam_Science/Assets/Scripts/CameraBehaviour.cs
+++ GameJam_Science/Assets/Scripts/CameraBehaviour.cs
@@ -2,24 +2,18 @@
 using System.Collections;
 
 public class CameraBehaviour : MonoBehaviour {
+	
 	public Transform target;
-	//public float smoothing = 5f;
-	public float speed = 20f;
-
-	private Vector3 offset;
+	public float speed = 10f;
 
 	// Use this for initialization
 	void Start () {
-		//offset = transform.position - target.position;
 		Vector3 trPos = new Vector3 (target.position.x, target.position.y, -10);
 		transform.position = trPos;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		//Vector3 targetCameraPosition = target.position + offset;
-		//transform.position = Vector3.Lerp (transform.position,targetCameraPosition,smoothing * Time.deltaTime);
-
 		transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed, 0f);
 	}
 }
