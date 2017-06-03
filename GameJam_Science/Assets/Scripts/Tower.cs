7e82ad1464ed38aa61e6c1aa7a766123f06abe70
Anglain <shudra.ia@gmail.com>
2017-06-03 21:05:57 +0300

    Minor tower sprites changes

--- GameJam_Science/Assets/Scripts/Tower.cs
+++ GameJam_Science/Assets/Scripts/Tower.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class Tower : Interactive {
 
+	public Sprite calmState;
+	public Sprite activatedState;
+	public Sprite deactivatedState;
+
 	// Use this for initialization
 	public override void Start () {
 		base.Start ();
@@ -10,7 +14,12 @@ public class Tower : Interactive {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown (KeyCode.Alpha1))
+			GetComponent<SpriteRenderer> ().sprite = calmState;
+		else if (Input.GetKeyDown(KeyCode.Alpha2))
+			GetComponent<SpriteRenderer> ().sprite = activatedState;
+		else if (Input.GetKeyDown(KeyCode.Alpha3))
+			GetComponent<SpriteRenderer> ().sprite = deactivatedState;
 	}
 
 	public override void OnCantMove<T>(T component) {
